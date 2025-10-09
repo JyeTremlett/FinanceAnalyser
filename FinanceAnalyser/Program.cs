d37a871004bd15b97083d512d0df7c5044db5309
jyetr <jyetr@MEIKASPC>
2025-10-09 17:04:13 +0800

    Add project files.

---
+++ FinanceAnalyser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
