729fc84aaae45b486f193abbf50ac5707543ddac
JyeTremlett <jyetremlett1999@gmail.com>
2025-10-21 18:16:41 +0800

    Added user secrets

--- FinanceAnalyser/Program.cs
+++ FinanceAnalyser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using FinanceAnalyser.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,8 +14,16 @@ namespace FinanceAnalyser
             builder.Services.AddSingleton<FlowController>();
             builder.Services.AddScoped<ICsvReaderService, CsvReaderService>();
 
-            // Start app
             var host = builder.Build();
+
+            // Retrieve App Secrets
+            IConfiguration config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            string fakeTestToken = config.GetValue<string>("fakeTestToken") ?? throw new InvalidOperationException("Could not get user secrets");
+
+            // Start app
             var app = host.Services.GetRequiredService<FlowController>();
             app.StartFlow();
         }
