e66a40b9728a8b476a4b816e35535fbc769a2b35
JyeTremlett <jyetremlett1999@gmail.com>
2026-02-07 09:46:36 +0800

    Added mudblazor

--- FinanceAnalyser.Web/Program.cs
+++ FinanceAnalyser.Web/Program.cs
@@ -1,4 +1,5 @@
 using FinanceAnalyser.Web.Components;
+using MudBlazor.Services;
 
 namespace FinanceAnalyser.Web
 {
@@ -11,6 +12,8 @@ namespace FinanceAnalyser.Web
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+            builder.Services.AddMudServices();
 
             var app = builder.Build();
 
